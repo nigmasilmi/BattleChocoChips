58458ec66a96388f0c4df7231eacb6c58d336748
nigmasilmi <nigmasilmi.web@gmail.com>
2019-11-28 11:40:28 -0300

    fixed the angularfire imports and cleaned up the app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayerAComponent } from './components/player-a/player-a.component';
@@ -16,7 +20,9 @@ import { GameBoardComponent } from './components/game-board/game-board.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
