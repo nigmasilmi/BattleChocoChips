ce1faf6643faefe0df8eaaa36cf4b1884c8affb2
nigmasilmi <nigmasilmi.web@gmail.com>
2019-11-29 14:18:52 -0300

    made functionality that creates board, fills with objects and properties, saves in firestore as document

--- src/app/components/player-a/player-a.component.ts
+++ src/app/components/player-a/player-a.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { FleetPlacingService } from '../../services/fleet-placing.service';
 
 @Component({
   selector: 'app-player-a',
   templateUrl: './player-a.component.html',
   styleUrls: ['./player-a.component.css']
 })
-export class PlayerAComponent implements OnInit {
+export class PlayerAComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('paboard', { static: false }) plABoard: ElementRef;
+  // permissionToRender = false;
+  permissionToRender = true;
+  board = [];
+  withCookie = false;
+  // rowInput: number;
+  // colInput: number;
+  colInput = 4;
+  rowInput = 4;
+  constructor(private fleetPlacingS: FleetPlacingService) { }
 
   ngOnInit() {
+    this.fleetPlacingS.createBoard(this.rowInput, this.colInput);
   }
 
+  ngAfterViewInit() {
+    console.log('nothing yet');
+  }
+  // function that sets the styles for occupied or not depending of the boolean value at the moment
+  setTheSquare() {
+    const classes = {
+      ocuppied: this.withCookie === true,
+      empty: !this.withCookie === true
+    };
+    return classes;
+  }
 }
--- src/app/services/fleet-placing.service.ts
+++ src/app/services/fleet-placing.service.ts
@@ -1,9 +1,61 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FleetPlacingService {
 
-  constructor() { }
+  board = {};
+
+  constructor(private afs: AngularFirestore) { }
+
+  createBoard(rows: number, columns: number) {
+    // create a 2d array that represents the player board with the number of c and r as arguments
+    // this.board = Array(rows).fill(Array(columns));
+    // tslint:disable-next-line: prefer-for-of
+    for (let r = 0; r < rows; r++) {
+      this.board[r] = [];
+      for (let c = 0; c < columns; c++) {
+        this.board[r][c] = {
+          coordinates: `${r}${c}`,
+          withCookie: this.placeCookieOrNot(),
+          hitted: 0,
+          eaten: 0
+        };
+      }
+
+    }
+    // llamar a función para guardar en firestore
+    const dataToSave = this.board;
+    this.saveBoardInFirestore(dataToSave);
+  }
+
+
+  // fuction that assigns 1 or 0 1 for placing cookie, 0 for not placing it
+  placeCookieOrNot() {
+    const numberA = Math.random();
+    const numberB = Math.random();
+    if (numberA > numberB) {
+      return 0;
+    } else {
+      return 1;
+    }
+  }
+
+  saveBoardInFirestore(data) {
+    return new Promise<any>((resolve, reject) => {
+      this.afs
+        .collection('boards')
+        .add(data)
+        .then(res => {
+          { }
+          console.log('esto es el resolve de la promesa en saveBoardInFirestore: ', res);
+        },
+          err => reject(err));
+    });
+  }
+
 }
